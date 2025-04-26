0139cc0d95f93497b23e582aab5e08bbbcc2c6b5
RiskWise-Project <riskwise.project@gmail.com>
2025-04-26 17:10:42 +0800

    RiskWise v1.0.0 --Fixed the initial commit--

---
+++ frontend/src/assets/logos/logo.jsx
@@ -0,0 +1,5 @@
+import riskwise_symbol from './RiskWise_Symbol.png'
+import riskwise_wordmark from './RiskWise_WordMark.png'
+import riskwise_combination from './RiskWise_Combination.png'
+
+export { riskwise_symbol, riskwise_wordmark, riskwise_combination }
