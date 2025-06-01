11bffeab68c6989ab803a8d2ab768f7a20ee1668
RiskWise-Project <riskwise.project@gmail.com>
2025-06-01 19:30:17 +0800

    RiskWise v1.0.5 --Fixed some ui and added lucide react for icons--

--- frontend/src/components/landingpage-components/about-section-component/about-lp-component.jsx
+++ frontend/src/components/landingpage-components/about-section-component/about-lp-component.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./about-lp-component.css";
+import { ArrowRight } from "lucide-react";
 
 function AboutSectionComponent() {
   return (
     <div className="about-main-container md:w-[90%] w-full m-auto px-5">
-      <div className="flex flex-col items-center gap-6 md:my-[10%] my-[50%]">
+      <div className="flex flex-col items-center gap-6 md:my-[5%] my-[25%]">
         <h1 className="font-black md:text-4xl text-3xl tracking-wider">
           ABOUT
         </h1>
-        <p className="text-center md:w-[50%] w-full md:text-xl text-[16px]">
+        <p className="text-justify md:text-center md:w-[50%] w-full md:text-xl text-[16px]">
           RiskWise is a tool for uncovering and handling risks. It runs on any
           device with a browser. You can tap into it even without an internet
           connection. Users can flag hazards, trace risks, and absorb safety
@@ -17,9 +18,9 @@ function AboutSectionComponent() {
         </p>
         <a
           href=""
-          className="md:text-xl text-[16px] mt-5 hover:text-[var(--color-highlight)] transition-colors duration-75 ease-in"
+          className="md:text-xl text-[16px] mt-5 hover:text-[var(--color-highlight)] flex flex-row items-center gap-2 transition-colors duration-75 ease-in"
         >
-          Learn More
+          Learn More <ArrowRight />
         </a>
       </div>
     </div>
--- frontend/src/components/landingpage-components/hero-section-components/hero-section-components.jsx
+++ frontend/src/components/landingpage-components/hero-section-components/hero-section-components.jsx
@@ -8,14 +8,21 @@ function HeroSectionComponent() {
       className="min-h-screen bg-cover bg-center"
       style={{ backgroundImage: `url(${heroBg})` }}
     >
-      <div className="header-text-container items-center md:items-start flex flex-col md:pt-[12%] pt-[70%] md:pl-[15%] pl-[1%] h-fit">
-        <h1 className="font-black md:text-6xl text-5xl tracking-wider">
-          RiskWise
+      <div className="header-text-container md:items-start flex flex-col md:pt-[12%] pt-[70%] md:pl-[15%] px-[5%] h-fit">
+        <h1 className="font-black md:text-5xl text-4xl tracking-wider leading-snug md:w-[60%] w-full">
+          RiskWise:{" "}
+          <span className="text-[var(--color-highlight)]">
+            {" "}
+            Real-Time Risk Assessment and Incident
+          </span>
         </h1>
 
-        <p className="text-[16px] md:text-2xl mt-3 md:w-[50%] w-[70%] text-center md:text-start">
-          A Progressive Web App for Real-Time Risk Assessment and Incident
-          Reporting.
+        <p className="text-[16px] md:text-xl mt-3 md:w-[50%] w-[100%] text-justify">
+          RISKWISE is a PWA designed to help organizations or communities
+          identify, assess, and manage risks in real-time. The system allows
+          users to report potential hazards, track ongoing risks, and receive
+          guidance on mitigation strategies — all from their mobile or desktop
+          browser, even offline.
         </p>
 
         <button className="md:mt-[5rem] mt-[3rem] bg-[var(--color-highlight)] md:px-9 px-6 md:py-2 py-1.5 rounded-md text-[var(--color-white)] md:text-lg text-[17px] hover:cursor-pointer get-started-btn">
