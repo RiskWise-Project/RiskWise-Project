b0afa80a9dff0dbe2bf324928b9c8e5a9411b943
RiskWise-Project <riskwise.project@gmail.com>
2025-06-03 15:23:56 +0800

    RiskWise v1.0.5 --Added download section and make it responsive--

---
+++ frontend/src/components/landingpage-components/download-section-component/download-component.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+function DownloadComponent() {
+  const [installPromptEvent, setInstallPromptEvent] = useState(null);
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (event) => {
+      event.preventDefault();
+      setInstallPromptEvent(event);
+    };
+
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+    };
+  }, []);
+
+  const handleInstallClick = () => {
+    if (installPromptEvent) {
+      installPromptEvent.prompt();
+      installPromptEvent.userChoice.then((choiceResult) => {
+        console.log(choiceResult.outcome);
+        setInstallPromptEvent(null);
+      });
+    }
+  };
+
+  return (
+    <div className="download-component-container flex flex-col items-center py-20 gap-5 px-5">
+      <h1 className="font-black md:text-4xl text-3xl tracking-wider mb-10">
+        DOWNLOAD
+      </h1>
+      <p className="text-center md:w-[50%] w-full md:text-xl text-[16px]">
+        Available on all major platforms. Get the app and start improving safety
+        today.
+      </p>
+      {installPromptEvent && (
+        <button
+          className="bg-[var(--color-highlight)] md:px-9 px-6 md:py-2 py-1.5 md:w-fit w-full rounded-md text-[var(--color-white)] md:text-lg text-[17px] hover:cursor-pointer get-started-btn"
+          onClick={handleInstallClick}
+        >
+          Install App
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default DownloadComponent;
--- frontend/src/pages/user-side-pages/landing-page.jsx
+++ frontend/src/pages/user-side-pages/landing-page.jsx
@@ -3,6 +3,7 @@ import HeaderComponent from "../../components/landingpage-components/header-comp
 import HeroSectionComponent from "../../components/landingpage-components/hero-section-components/hero-section-components";
 import AboutSectionComponent from "../../components/landingpage-components/about-section-component/about-lp-component";
 import FeatureComponent from "../../components/landingpage-components/features-section-component/features-component";
+import DownloadComponent from "../../components/landingpage-components/download-section-component/download-component";
 
 function LandingPage() {
   return (
@@ -18,6 +19,9 @@ function LandingPage() {
       </div>
       <div id="feature" className="feature-component-container">
         <FeatureComponent />
+      </div>
+      <div id="download" className="download-component-container">
+        <DownloadComponent />
       </div>
     </div>
   );
