3dd26a29bddb35db26d4ccb9fc5ae096e8adde8d
RiskWise-Project <riskwise.project@gmail.com>
2025-06-14 18:53:23 +0800

    RiskWise v1.0.8 --Already done the functionalities of the concerns, and everything in the UI is already fixed--////

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 require("dotenv").config({ path: ".env.development" });
 const ConcernRoutes = require("../routes/concerns-routes");
 const cors = require("cors");
+const compression = require("compression");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/", ConcernRoutes);
+
+app.use(compression());
+app.use(express.static(path.resolve(__dirname, "dist")));
 
 const PORT = process.env.PORT || 3000;
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,33 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
+import compression from "vite-plugin-compression";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    compression({
+      algorithm: "brotliCompress", // or 'gzip'
+      ext: ".br", // or '.gz'
+      deleteOriginFile: false, // keep original files
+    }),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
+
       manifest: {
-        name: 'RiskWise',
+        name: "RiskWise",
         description:
-          'Report and manage risks easily with real-time updates and offline support.',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#1976d2',
-        orientation: 'portrait-primary',
-        lang: 'en',
+          "Report and manage risks easily with real-time updates and offline support.",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#1976d2",
+        orientation: "portrait-primary",
+        lang: "en",
         icons: [
           {
-            src: '/icon/RiskWise_Symbol.png',
-            sizes: '500x500',
-            type: 'image/png',
-            purpose: 'any',
+            src: "/icon/RiskWise_Symbol.png",
+            sizes: "500x500",
+            type: "image/png",
+            purpose: "any",
           },
         ],
       },
     }),
   ],
-})
+});
