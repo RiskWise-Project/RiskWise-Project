e7444dfb89771f98410c5428f23debb9bdbb3736
RiskWise-Project <riskwise.project@gmail.com>
2025-06-15 10:11:31 +0800

    RiskWise v1.0.9 --Fixing the deployment of the backend--/

--- backend/helper/firebase.js
+++ backend/helper/firebase.js
@@ -1,10 +1,23 @@
 const admin = require("firebase-admin");
 const path = require("path");
 
-const serviceAccount = require(path.resolve(
-  __dirname,
-  "../riskwise-17990-firebase-adminsdk-fbsvc-147830006d.json"
-));
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+} else if (process.env.FIREBASE_SERVICE_ACCOUNT_JSON_B64) {
+  const decoded = Buffer.from(
+    process.env.FIREBASE_SERVICE_ACCOUNT_JSON_B64,
+    "base64"
+  ).toString("utf-8");
+  serviceAccount = JSON.parse(decoded);
+} else {
+  const path = require("path");
+  serviceAccount = require(path.resolve(
+    __dirname,
+    "../riskwise-17990-firebase-adminsdk-fbsvc-147830006d.json"
+  ));
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
