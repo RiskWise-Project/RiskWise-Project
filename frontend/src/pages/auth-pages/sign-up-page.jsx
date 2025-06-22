1537c6f0281fe6baace281045093a28cd9a4efc8
RiskWise-Project <riskwise.project@gmail.com>
2025-06-22 17:12:12 +0800

    RiskWise v1.2.3 --Started implementing the users auth, and currently checking for bugs and errors--

--- frontend/src/pages/auth-pages/sign-up-page.jsx
+++ frontend/src/pages/auth-pages/sign-up-page.jsx
@@ -36,15 +36,7 @@ function SignUpPage() {
           />
         </div>
         <div className="form-container flex flex-col items-center justify-center w-full h-full">
-          <Suspense
-            fallback={
-              <div className="w-full h-full flex items-center justify-center">
-                Loading...
-              </div>
-            }
-          >
-            <SignUpForm />
-          </Suspense>
+          <SignUpForm />
         </div>
       </div>
       <div className="right-side-container-signup flex items-center justify-center bg-[var(--color-highlight)] ">
