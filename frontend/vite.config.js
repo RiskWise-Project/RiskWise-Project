3d3a85df12f6faafde55ffbc225f2e5830ddc194
RiskWise-Project <riskwise.project@gmail.com>
2025-11-24 21:12:59 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,28 +15,32 @@ export default defineConfig({
     }),
 
     VitePWA({
-      registerType: "autoUpdate",
+  registerType: "autoUpdate",
 
-      manifest: {
-        name: "RiskWise",
-        description:
-          "Report and manage risks easily with real-time updates and offline support.",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#1976d2",
-        orientation: "portrait-primary",
-        lang: "en",
-        icons: [
-          {
-            src: "/icon/RiskWise_Symbol.png",
-            sizes: "500x500",
-            type: "image/png",
-            purpose: "any",
-          },
-        ],
+  workbox: {
+    navigateFallbackDenylist: [/\/__/],
+  },
+
+  manifest: {
+    name: "RiskWise",
+    description:
+      "Report and manage risks easily with real-time updates and offline support.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#1976d2",
+    orientation: "portrait-primary",
+    lang: "en",
+    icons: [
+      {
+        src: "/icon/RiskWise_Symbol.png",
+        sizes: "500x500",
+        type: "image/png",
+        purpose: "any",
       },
-    }),
+    ],
+  },
+}),
   ],
   build: {
     target: "esnext",
